eff861b133793c392f0cd16b51a49edf18575ead
Adrian nathanael <115018250+AdrianNael@users.noreply.github.com>
2024-02-09 14:33:32 +0700

    services categories

--- app/api/v1/categories/controller.js
+++ app/api/v1/categories/controller.js
@@ -1,30 +1,19 @@
-// import model category
-const Categories = require("./model");
+// import services categories
+const {
+  getAllCategories,
+  getOneCategories,
+  updateCategories,
+  createCategories,
+  deleteCategories,
+} = require('../../../services/mongoose/categories');
 
-// buat function create
-const create = async (req, res, next) => {
+const { StatusCodes } = require('http-status-codes');
+
+const create = async (req, res) => {
   try {
-    // membuat categories baru menggunakan data dari `name`
-    const { name } = req.body;
+    const result = await createCategories(req);
 
-    // simpan Category yang baru dibuat ke MongoDB
-    const result = await Categories.create({ name });
-
-    // berikan response kepada client dengan mengembalikan product yang baru dibuat
-    res.status(201).json({
-      data: result,
-    });
-  } catch (err) {
-    // jika terjadi kesalahan kemudian gunakan method `next` agar Express memproses error tersebut
-    next(err);
-  }
-};
-
-// get all data
-const index = async (req, res, next) => {
-  try {
-    const result = await Categories.find();
-    res.status(200).json({
+    res.status(StatusCodes.CREATED).json({
       data: result,
     });
   } catch (err) {
@@ -32,19 +21,11 @@ const index = async (req, res, next) => {
   }
 };
 
-const find = async (req, res, next) => {
+const index = async (req, res) => {
   try {
-    const { id } = req.params;
+    const result = await getAllCategories(req);
 
-		// mencari categories di MongoDB berdasarkan field _id
-    const result = await Categories.findOne({ _id: id });
- 
-    // bila result tidak mendapatkan data categories maka akan mereturn response `message: 'Id categories tidak ditemukan'`
-    if (!result) {
-      return res.status(404).json({ message: 'Id categories tidak ditemukan' });
-    }
-
-    res.status(200).json({
+    res.status(StatusCodes.OK).json({
       data: result,
     });
   } catch (err) {
@@ -52,19 +33,11 @@ const find = async (req, res, next) => {
   }
 };
 
-const update = async (req, res, next) => {
+const find = async (req, res) => {
   try {
-    const { id } = req.params;
-    const { name } = req.body;
+    const result = await getOneCategories(req);
 
-    // cari dan update categories berdasarkan field _id
-    const result = await Categories.findOneAndUpdate(
-      { _id: id },
-      { name },
-      { new: true, runValidators: true } // menampilkan data baru dan menjalankan validation
-    );
-
-    res.status(200).json({
+    res.status(StatusCodes.OK).json({
       data: result,
     });
   } catch (err) {
@@ -72,11 +45,23 @@ const update = async (req, res, next) => {
   }
 };
 
-const destroy = async (req, res, next) => {
+const update = async (req, res) => {
   try {
-    const { id } = req.params;
-    const result = await Categories.findByIdAndDelete(id);
-    res.status(200).json({
+    const result = await updateCategories(req);
+
+    res.status(StatusCodes.OK).json({
+      data: result,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const destroy = async (req, res) => {
+  try {
+    const result = await deleteCategories(req);
+
+    res.status(StatusCodes.OK).json({
       data: result,
     });
   } catch (err) {
@@ -85,9 +70,9 @@ const destroy = async (req, res, next) => {
 };
 
 module.exports = {
-  index, 
-  create, 
+  index,
   find,
   update,
-  destroy
-}
+  destroy,
+  create,
+};
